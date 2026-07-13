8a9b6c62564bf7f53a7e8a0315f22313c1719eb2
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-07-12 23:18:36 -0600

    0

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-// Registro del Service Worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(reg => console.log('SW registrado', reg))
-            .catch(err => console.error('Error al registrar SW', err));
-    });
+    // Si estás en local usa '/', si estás en GitHub Pages usa '/Colegios/'
+    const path = window.location.hostname === 'localhost' ? '/sw.js' : '/Colegios/sw.js';
+    navigator.serviceWorker.register(path)
+        .then(reg => console.log('SW registrado correctamente en:', path))
+        .catch(err => console.error('Error al registrar SW', err));
 }
