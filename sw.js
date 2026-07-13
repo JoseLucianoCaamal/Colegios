4afd58166d7fc88325979815402c23e6dd073760
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-07-12 23:20:33 -0600

    0

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
 const CACHE_NAME = 'colegio-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/app.js',
-  '/manifest.json',
-  '/Img/logo.png' 
+  '/Colegios/',
+  '/Colegios/index.html',
+  '/Colegios/css/styles.css',
+  '/Colegios/js/app.js',
+  '/Colegios/manifest.json',
+  '/Colegios/Img/logo.png' 
 ];
 
 // Instalar el Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      // Usamos un bucle para añadir archivos y que falle solo el que no encuentra
+      return Promise.all(
+        ASSETS.map(url => cache.add(url).catch(err => console.error('No se pudo cachear:', url)))
+      );
     })
   );
 });
 
-// Interceptar peticiones y servir desde caché
+// Interceptar peticiones
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
