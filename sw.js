2fec41d1d92bbaebb852e2844a6ae98aa340fb17
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-07-14 18:56:43 -0600

    4.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambiamos a v2 para forzar la actualización
-const CACHE_NAME = 'colegio-v4.0'; 
+const CACHE_NAME = 'colegio-v4.1'; 
 const ASSETS = [
   '/Colegios/',
   '/Colegios/index.html',
