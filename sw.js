57195623b1fdaf81a890c4f99ad17c725368b062
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-08-25 16:05:26 -0600

    9.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambiamos la versión para forzar la actualización en todos los celulares
-const CACHE_NAME = 'colegio-v10.4';
+const CACHE_NAME = 'colegio-v10.5';
 const ASSETS = [
   '/Colegios/',
   '/Colegios/index.html',
